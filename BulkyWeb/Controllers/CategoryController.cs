eb253330c994ec0a78304a8aedfcc1db94723062
mayankmandal <mayankmandal077@gmail.com>
2023-09-03 11:16:06 +0530

    Added Cosmetic Changes for Create Category View

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name","The Display Order cannot match with Category Name");
+            }
+            if (obj.Name!=null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is a invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+    }
+}
